fff45fa02b9c1b25babe165f72787088c77a2302
Primoz Padar <primoz.padar1@gmail.com>
2021-10-19 21:24:47 +0200

    init

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+</script>
+
+<h1 class="title">realistic shadow</h1>
+
+<style lang="less" global>
+  :root {
+    --teal: rgb(0, 173, 181);
+    --grey: rgb(57, 62, 70);
+    --grey-dark: #222831;
+    --white: rgb(255, 255, 255);
+  }
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+  }
+
+  body {
+    margin: 0;
+  }
+
+  .title {
+    color: var(--teal);
+  }
+</style>
